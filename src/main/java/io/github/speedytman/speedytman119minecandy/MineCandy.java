0917eec021e3943b79542fde548d793adc9834f1
speedytman <wdcoker3@gmail.com>
2022-11-22 13:38:23 -0500

    Initial Commit

---
+++ src/main/java/io/github/speedytman/speedytman119minecandy/MineCandy.java
@@ -0,0 +1,8 @@
+package io.github.speedytman.speedytman119minecandy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(MineCandy.MODID)
+public class MineCandy {
+    public static final String MODID = "speedytman119minecandy";
+}
