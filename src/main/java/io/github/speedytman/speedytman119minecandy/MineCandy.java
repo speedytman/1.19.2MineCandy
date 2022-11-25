47446e3a7b1b7eb0c0d5cd1e74508949d09c802a
speedytman <wdcoker3@gmail.com>
2022-11-25 00:14:24 -0500

    updated mods.toml, updated build.gradle, added boilerplate code to MineCandy.java

--- src/main/java/io/github/speedytman/speedytman119minecandy/MineCandy.java
+++ src/main/java/io/github/speedytman/speedytman119minecandy/MineCandy.java
@@ -1,8 +1,39 @@
 package io.github.speedytman.speedytman119minecandy;
 
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
 
 @Mod(MineCandy.MODID)
 public class MineCandy {
     public static final String MODID = "speedytman119minecandy";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MineCandy() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
 }
